b1349ab103c8fb43ad8ea973caca5a7ea99d6827
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-04 21:34:37 -0300

    Add frontend react basic structure

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Provider from './providers/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
