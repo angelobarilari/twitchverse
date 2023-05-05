22fc295bfcd9ffefd24bc0ab057f09932e24f9d3
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-04 21:00:29 -0300

    fix: fixed some thing in bot

--- frontend/src/components/Button/index.jsx
+++
@@ -1,7 +0,0 @@
-import { StyledButton } from "./styles";
-
-const Button = ({ children, ...rest }) => {
-  return <StyledButton {...rest}>{children}</StyledButton>
-};
-
-export default Button
--- frontend/src/services/api.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const apiUrl = axios.create({
-  baseURL: "http://localhost:8000/api/messages/"
-});
-
-export default apiUrl
