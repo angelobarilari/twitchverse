1c297d469a423b513337aa6fb6c5429a040fbf81
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-08 18:05:09 -0300

    chore: removed print and removed unused route

--- backend/customapi/settings.py
+++ backend/customapi/settings.py
@@ -154,7 +154,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
 }
 
 TIME_ZONE = "America/Sao_Paulo"
