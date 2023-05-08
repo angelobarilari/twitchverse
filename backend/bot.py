b33f662fb47ac68b3bdea1dcf15afa4b804235e0
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-08 11:26:27 -0300

    feat: add pre-commit and moved backend files to backend folder

--- backend/bot.py
+++ backend/bot.py
@@ -6,18 +6,22 @@ import dotenv
 
 dotenv.load_dotenv()
 
+
 async def verse_generation(text):
     openai.api_key = os.getenv("OPENAPI_KEY")
 
     verse = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=[{
-            "role": "user", 
-            "content": f"Crie um verso que rima até 3 linhas usando a seguinte mensagem: {text}"
-        }]
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": f"Crie um verso que rima até 3 linhas usando a seguinte mensagem: {text}",
+            }
+        ],
     )
 
     return verse.choices[0].message.content
+
 
 # Database connection
 async def db_connection():
@@ -25,49 +29,53 @@ async def db_connection():
         user="postgres",
         password="1234",
         database="twitchbot",
-        host="localhost"
+        host="localhost",
     )
 
     return connection
 
+
 # This function persists the messages in the database when called
 async def store_message(connection, msg, generated_verse):
-    query = ''' INSERT INTO datacollector_message 
+    query = """ INSERT INTO datacollector_message 
                     (id, author, original_message, timestamp, channel, color, generated_verse) 
                 VALUES 
                     ($1, $2, $3, $4, $5, $6, $7)
-            '''
-    
+            """
+
     await connection.execute(
-        query, 
-        msg.id, 
-        msg.author.name, 
-        msg.content, 
-        msg.timestamp, 
+        query,
+        msg.id,
+        msg.author.name,
+        msg.content,
+        msg.timestamp,
         msg.channel.name,
-        msg.tags['color'],
-        generated_verse
+        msg.tags["color"],
+        generated_verse,
     )
+
 
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(
             # Put your OAuth Password Token here. You can obtain one in https://twitchapps.com/tmi/
-            token="oauth:jybeok26b7qi2in16uha11n34xqrf6", 
-            prefix='!', 
+            token="oauth:jybeok26b7qi2in16uha11n34xqrf6",
+            prefix="!",
             # Set channels to track here
-            initial_channels=['darionpk'])
+            initial_channels=["darionpk"],
+        )
 
         self.db_connection = None
 
     async def event_ready(self):
         self.db_connection = await db_connection()
-        print('Bot is running')
+        print("Bot is running")
 
     async def event_message(self, message):
         print(message.content)
         generated_verse = await verse_generation(message.content)
         await store_message(self.db_connection, message, generated_verse)
+
 
 bot = Bot()
 bot.run()
--- backend/datacollector/models.py
+++ backend/datacollector/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 import uuid
 
+
 class Message(models.Model):
     id = models.UUIDField(
-        primary_key = True,
-        default = uuid.uuid4,
-        editable = False,
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
     )
     author = models.CharField(max_length=255)
     channel = models.CharField(max_length=255)
