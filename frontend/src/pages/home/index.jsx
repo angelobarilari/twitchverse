9e7e2a80d3ba75cfcc90340d872ad45fdc8b2514
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-08 14:57:42 -0300

    feat: search by username bar implement and yup schema

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,14 @@
-import Header from '../../components/Header'
-import { StyledDefaultPage } from '../../components/DefaultPage/style'
-import { MessageDataContext } from '../../providers/messages'
 import { useContext } from "react"
-import apiUrl from '../../services/api'
+import * as yup from 'yup'
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import { MessageDataContext } from '../../providers/messages'
+import { StyledDefaultPage } from '../../components/DefaultPage/style'
+import Header from '../../components/Header'
 import Message from '../../components/Message'
 import Button from '../../components/Button'
 import Box from '../../components/Box'
+import apiUrl from '../../services/api'
 import "./style.css"
 
 function Home() {
@@ -13,19 +16,67 @@ function Home() {
 
     const messagesData = () => {
         apiUrl.get()
-             .then(res => {
-                setMessages(messages.concat(res.data.results))
-            })
-             .catch(err => {
-                console.log(err)
-            })
+             .then(res => setMessages(messages.concat(res.data.results)))
+             .catch(err => console.log(err))
     }
+
+    const messagesByUsername = (formData) => {
+        apiUrl.get(`${formData.username}/`)
+            .then(res => setMessages(messages.concat(res.data.results)))
+            .catch(err => console.log(err))
+    }
+
+    const requestMessagesByUsernameSchema = yup.object().shape({
+        username: yup.string()
+                     .required('Enter a username')
+    })
     
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(requestMessagesByUsernameSchema)
+    })
+
     return (
         <>  
             <Header />
 
-            <StyledDefaultPage>
+            <StyledDefaultPage
+                id="home-page">
+                    
+                <Box
+                    id="search-box"
+                    minWidth="500px"
+                    maxWidth="30%"
+                    height="fit-content%"
+                    background="var(--grey-3)">
+
+                    <form
+                        onSubmit={handleSubmit(messagesByUsername)}>
+                        
+                        <div className="input-container">
+                                <input 
+                                    className="search-input"
+                                    type="text"
+                                    placeholder="Search messages by username"
+                                    {...register("username")} />
+                        </div>
+                        
+                        <Button 
+                            id="submit-btn"
+                            minWidth="20%"
+                            height="80%"
+                            background="var(--purple-1)" 
+                            color="var(--white)" 
+                            hover="var(--purple-2)"
+                            type="submit">  
+                            Search
+                        </Button>
+                    </form>
+                    {errors.username?.message && 
+                        <span className="error-message">
+                            {errors.username.message}
+                        </span>}
+                </Box>
+
                 <Box 
                     className="chat-box"
                     minWidth="400px"
@@ -91,9 +142,6 @@ function Home() {
                         type="submit"
                         onClick={() => messagesData()} />
                 </Box>
-            
-            
-
             </StyledDefaultPage>
         </>
     )
