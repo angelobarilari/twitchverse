f0e510f8cbb672fec0f348e89df2a7ca09777624
angelobarilari <angelomelo_tdai@hotmail.com>
2023-05-09 17:47:44 -0300

    chore: added env vars in docker-compose.yml

--- backend/customapi/settings.py
+++ backend/customapi/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = "customapi.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB_NAME"),
-        "USER": os.getenv("POSTGRES_USERNAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_DB_PORT"),
