d7e55932369dea51186930b0f27777aa35681520
angelobarilari <98756174+angelobarilari@users.noreply.github.com>
2023-05-11 14:52:05 -0300

    Update settings.py

--- backend/customapi/settings.py
+++ backend/customapi/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = "customapi.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+        "NAME": "twitchbot",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "db",
+        "PORT": "5432",
     },
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
